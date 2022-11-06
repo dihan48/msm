10c810da6fb05b6665c3fbe506011318f9de3227
dihan48 <dihan4894@mail.ru>
2022-11-06 13:23:22 +0300

    секция 4

--- components/Section4.js
+++ components/Section4.js
@@ -1,8 +1,23 @@
+import Image from "next/image";
 import styles from "../styles/Section4.module.css";
 
 export function Section4() {
   return (
     <section className={styles.container}>
+      <Image
+        src={"/bg4_1.png"}
+        alt=""
+        width={414}
+        height={325}
+        className={styles.bg1}
+      />
+      <Image
+        src={"/bg4_2.png"}
+        alt=""
+        width={172}
+        height={182}
+        className={styles.bg2}
+      />
       <h1 className={styles.title}>
         Этапы <span className={styles.title_colored}>работы</span>
       </h1>
@@ -14,7 +29,9 @@ export function Section4() {
               Сбор информации, необходимой для начала работы.
             </p>
             <p className={styles.card_text}>
-              Изучаем вашу задачу
+              <span className={styles.card_text_highlight}>
+                Изучаем вашу задачу
+              </span>
               <br /> и сведения о товаре
             </p>
             <p className={styles.card_text}>
@@ -28,19 +45,35 @@ export function Section4() {
             <p className={styles.card_text}>
               Проводим комплексный
               <br />
-              анализ ниши и конкурентов
+              <span className={styles.card_text_highlight}>
+                анализ ниши и конкурентов
+              </span>
             </p>
             <p className={styles.card_text}>
-              Выделяем сильные стороны товара, <br />
+              <span className={styles.card_text_highlight}>
+                Выделяем сильные стороны товара
+              </span>
+              , <br />
               на которые следует обратить внимание потенциальных покупателей
             </p>
             <p className={styles.card_text}>
               На основании полученных данных
               <br />
-              создаем стильный дизайн,
+              <span className={styles.card_text_highlight}>
+                создаем стильный дизайн
+              </span>
+              ,
               <br />
-              который привлекает внимание покупателя
-              <br />и выгодно выделяет ваш товар <br />
+              который{" "}
+              <span className={styles.card_text_highlight}>
+                привлекает внимание
+              </span>{" "}
+              покупателя
+              <br />и{" "}
+              <span className={styles.card_text_highlight}>
+                выгодно выделяет ваш товар
+              </span>{" "}
+              <br />
               на фоне карточек конкурентов
             </p>
           </div>
@@ -49,14 +82,20 @@ export function Section4() {
           <h2 className={styles.card_title}>Утверждение и сдача</h2>
           <div className={styles.card_content3}>
             <p className={styles.card_text}>
-              После выполнения карточка презентуется заказчику
+              <span className={styles.card_text_highlight}>
+                После выполнения карточка презентуется заказчику
+              </span>
             </p>
             <p className={styles.card_text}>
-              Если дизайн нравиться
+              <span className={styles.card_text_highlight}>
+                Если дизайн нравиться
+              </span>
               <br />и устраивает клиента - отправляем архив с файлами проекта
             </p>
             <p className={styles.card_text}>
-              Если дизайн не подходит
+              <span className={styles.card_text_highlight}>
+                Если дизайн не подходит
+              </span>
               <br />и нужны правки - выясняем, что именно нужно изменить и
               дорабатываем
             </p>
