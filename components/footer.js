b91c7565250a1bcc19056dac7f145f14848f79d2
dihan48 <dihan4894@mail.ru>
2022-11-12 15:10:48 +0300

    rename fix

---
+++ components/footer.js
@@ -0,0 +1,49 @@
+import styles from "../styles/footer.module.css";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.info_container}>
+          <div className={styles.logo} />
+          <div className={styles.info}>
+            Маркетинговое агенство
+            <br />
+            ИП ИГЛИН АНТОН ПЕТРОВИЧ
+            <br />
+            ИНН 231222224400
+            <br />
+            ОГРН 322237500004779
+          </div>
+          <div className={styles.copyright}>© MSM.STORE 2022</div>
+        </div>
+        <div className={styles.links_container}>
+          <a href="#" className={styles.link}>
+            Главная
+          </a>
+          <a href="#" className={styles.link}>
+            Портфолио
+          </a>
+          <a href="#" className={styles.link}>
+            Преимущества
+          </a>
+          <a href="#" className={styles.link}>
+            Цена
+          </a>
+          <a href="#" className={styles.link}>
+            Отзывы
+          </a>
+        </div>
+        <div className={styles.socials_container}>
+          <a href="#" className={styles.tg_link} />
+          <a href="#" className={styles.vk_link} />
+          <h1 className={styles.title}>
+            <div className={styles.title_word}>Marketing</div>
+            <div className={styles.title_word}>Services</div>
+            <div className={styles.title_word}>Marketplace</div>
+          </h1>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ components/section8.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import styles from "../styles/section8.module.css";
+
+export function Section8() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.bg1}>
+        <Image
+          src={require("../public/Group4.svg")}
+          alt=""
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+          quality={100}
+        />
+      </div>
+      <div className={styles.bg2}>
+        <Image
+          src={require("../public/r11.svg")}
+          alt=""
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+          quality={100}
+        />
+      </div>
+      <div className={styles.bg3}>
+        <Image
+          src={require("../public/do.png")}
+          alt=""
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+          quality={100}
+        />
+      </div>
+      <div className={styles.bg4}>
+        <Image
+          src={require("../public/posle.png")}
+          alt=""
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+          quality={100}
+        />
+      </div>
+    </section>
+  );
+}
