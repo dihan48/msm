09be0c45ac8f9f861e2590694762074fa6470741
dihan48 <dihan4894@mail.ru>
2022-11-20 00:52:01 +0300

    "точки" для слайдера

--- components/section3.js
+++ components/section3.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
 import "swiper/css";
@@ -39,6 +39,7 @@ const slider = [
 ];
 
 export function Section3() {
+  const [swiper, setSwiper] = useState();
   const paginationRef = useRef();
   return (
     <div className={styles.bg_container}>
@@ -82,17 +83,21 @@ export function Section3() {
         <h1 className={styles.title}>Портфолио</h1>
         <div className={styles.slider_container}>
           <Swiper
+            onSwiper={setSwiper}
             className={styles.slider}
             spaceBetween={8}
             slidesPerView="auto"
+            centeredSlides={true}
             breakpoints={{
               979: {
                 slidesPerView: "auto",
                 slidesPerGroup: 1,
+                centeredSlides: true,
               },
               980: {
                 slidesPerView: 5,
                 slidesPerGroup: 5,
+                centeredSlides: false,
               },
             }}
           >
@@ -112,10 +117,67 @@ export function Section3() {
                 </SwiperSlide>
               ))}
           </Swiper>
+          <SwiperPagination swiper={swiper} />
         </div>
         <div ref={paginationRef} className={styles.pagination} />
         <button className={styles.button}>Смотреть все работы</button>
       </section>
     </div>
+  );
+}
+
+/*
+магические числа работают для 5 точке, смещение 1 точки 20px
+*/
+function getDotsOffset(dotsCount, currentDot) {
+  if (dotsCount > 5 && currentDot > 2) {
+    if (currentDot > dotsCount - 4) {
+      return (dotsCount - 5) * 23;
+    }
+    return (currentDot - 2) * 23;
+  }
+  return 0;
+}
+
+function SwiperPagination({ swiper }) {
+  const [slides, setSlides] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    if (swiper) {
+      const slideChangeHandle = function () {
+        setSlides(swiper.snapGrid);
+        setActiveIndex(swiper.snapIndex);
+        console.log(swiper);
+      };
+      slideChangeHandle();
+      swiper.on("slideChange", slideChangeHandle);
+      return () => swiper.off("slideChange", slideChangeHandle);
+    }
+  }, [swiper]);
+
+  return (
+    <>
+      <div className={styles.dots_container}>
+        <div
+          className={styles.dots}
+          style={{
+            left: `-${getDotsOffset(slides?.length, activeIndex)}px`,
+          }}
+        >
+          {slides?.map((child, index) => (
+            <div
+              key={index}
+              className={`${styles.dot} ${
+                swiper.snapIndex === index ? styles.active : ""
+              }`}
+              onClick={() => {
+                swiper.slideTo(index * swiper.params.slidesPerGroup);
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
